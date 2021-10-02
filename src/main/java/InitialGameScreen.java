df87aa6af549f9ee622ec43291cc49103710eb1d
amittal98 <amittal98@gatech.edu>
2021-10-02 15:15:09 -0400

    Init_screen

---
+++ src/main/java/InitialGameScreen.java
@@ -0,0 +1,66 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.FXGLIntroScene;
+import com.almasb.fxgl.app.scene.SceneFactory;
+import com.almasb.fxgl.app.scene.StartupScene;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
+
+import java.util.Map;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+
+public class InitialGameScreen extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        gameSettings.setWidth(1050);
+        gameSettings.setHeight(700);
+    }
+    @Override
+    protected void initGame() {
+        getGameScene().setBackgroundRepeat("Map_plaingrass.png");
+        moneyBox(getAppWidth()/2 - 100,0);
+    }
+
+    @Override
+    protected void initUI() {
+        Text money = getUIFactoryService().newText("", Color.BLACK, 22);
+        Text moneyText = getUIFactoryService().newText("Money: ", Color.BLACK, 22);
+        money.setTranslateX(getAppWidth()/2 + 25);
+        money.setTranslateY(25);
+
+        moneyText.setTranslateX(getAppWidth()/2 - 75);
+        moneyText.setTranslateY(25);
+
+        money.textProperty().bind(getWorldProperties().intProperty("money_count").asString());
+
+        getGameScene().addUINodes(money, moneyText);
+    }
+
+    //Update money_count to show the updated money
+    //The integer value is the initial money at setup.
+    //Modify it to represent change with the difficulty level.
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("money_count", 100);
+    }
+
+    //Just a background box
+    private Entity moneyBox(double x, double y) {
+        return entityBuilder()
+                .at(x, y)
+                .type(EntityType.MONEY)
+                .viewWithBBox(new Rectangle(200,50 , Color.GOLD))
+                .buildAndAttach();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
