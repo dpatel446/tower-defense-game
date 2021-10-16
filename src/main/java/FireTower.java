893b5291b161447438c8ef3e9b138095b63c783c
Awal, Abrar Ibne <aawal7@gatech.edu>
2021-10-16 13:14:43 -0400

    Add files via upload

---
+++ src/main/java/FireTower.java
@@ -0,0 +1,10 @@
+public class FireTower extends Tower {
+
+	public FireTower() {
+	}
+	
+	@Override
+	public String getDescription() {
+		return "A Tower made of fire, shooting fireballs at any enemy in sight";
+	}
+}
