8b64e2c4cc86251c40a70e2f75066fb4fef4cccc
Patel, Dhrushil Pratik <dpatel446@gatech.edu>
2021-11-04 12:14:17 -0400

    Tests the game's spawn and exit functionality

---
+++ src/test/java/TestSpawnandExit.java
@@ -0,0 +1,27 @@
+import javafx.geometry.Point2D;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestSpawnandExit {
+
+    private static boolean exit = false;
+    private Point2D origin = new Point2D(175, 45);
+
+    @Test
+    public void testExit() {
+        TowerDefense.setHealth(0);
+        boolean chooseExit = true;
+        if (!(TowerDefense.getHealth().intValue() > 0) && chooseExit) {
+            exit = true;
+        }
+        Assertions.assertTrue(exit);
+    }
+
+    @Test
+    public void testSpawn() {
+        Point2D enemy = new Point2D(175, 45);
+        TowerDefense.setIsStarted(true);
+        Assertions.assertEquals(enemy, origin);
+        Assertions.assertTrue(TowerDefense.getIsStarted());
+    }
+}
