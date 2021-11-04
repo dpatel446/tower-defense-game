fb63c65f8db003dacca14c1438957fc56ed47ceb
Awal, Abrar Ibne <aawal7@gatech.edu>
2021-11-04 10:51:45 -0400

    Add files via upload

---
+++ src/test/java/StartGameTest.java
@@ -0,0 +1,38 @@
+import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
+import javafx.scene.control.Button;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StartGameTest {
+
+    private static TowerDefense app = new TowerDefense();
+    private static boolean isStarted = true;
+
+    @Test
+    public void testStartGame() throws InterruptedException {
+        TestThread thread = new TestThread();
+        thread.start();
+        Thread.sleep(3000);
+
+        Button startGame = new Button("Start Game");
+        startGame.setTranslateX((getAppWidth() / 2) - 50);
+        startGame.setTranslateY(300);
+        app.startCombat();
+        assertEquals(isStarted, true);
+    }
+
+    @Test
+    public void testRestartGame() {
+        app.setIsStarted(true);
+        boolean isRestarted = app.getIsStarted();
+        assertEquals(isRestarted, true);
+    }
+
+    private static class TestThread extends Thread {
+        @Override
+        public void run() {
+            String[] str = {"hi"};
+            TowerDefense.main(str);
+        }
+    }
+}
