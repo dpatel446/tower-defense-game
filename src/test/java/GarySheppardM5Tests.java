0c08797571516539ccd05daaed603fb0bdfe9ccc
GarySheppard <gsheppard6@gatech.edu>
2021-11-17 13:29:37 -0500

    Added GarySheppardM5Tests.java to implement testEnemyAttacks() and testRandomEnemies

---
+++ src/test/java/GarySheppardM5Tests.java
@@ -0,0 +1,54 @@
+import com.almasb.fxgl.entity.Entity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.almasb.fxgl.dsl.FXGL.spawn;
+
+public class GarySheppardM5Tests {
+    @Test
+    public void testEnemyAttacks() {
+        TowerDefense.setHealth(100);
+
+        TowerDefense.attackMonument(2);
+        Assertions.assertEquals(98, TowerDefense.getHealth().intValue());
+
+        TowerDefense.attackMonument(20);
+        Assertions.assertEquals(78, TowerDefense.getHealth().intValue());
+
+        TowerDefense.attackMonument(5);
+        Assertions.assertEquals(73, TowerDefense.getHealth().intValue());
+    }
+
+    @Test
+    public void testRandomEnemies() {
+        Integer[] listEnemies = new Integer[10];
+
+        Random rand = new Random();
+        int enemyTicket = rand.nextInt(3);
+        for (int i = 0; i < 10; i++) {
+            Integer enemy = null;
+            switch (enemyTicket) {
+                case 0:
+                    enemy = 0;
+                    listEnemies[i] = enemy;
+                    break;
+                case 1:
+                    enemy = 1;
+                    listEnemies[i] = enemy;
+                    break;
+                case 2:
+                    enemy = 2;
+                    listEnemies[i] = enemy;
+                    break;
+            }
+        }
+
+        for (Integer enemy: listEnemies) {
+            Assertions.assertNotNull(enemy);
+        }
+    }
+}
