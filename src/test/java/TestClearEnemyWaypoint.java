560b717dbc9281171cc36413e7583a550bc0328a
Patel, Dhrushil Pratik <dpatel446@gatech.edu>
2021-11-18 11:09:00 -0500

    Add files via upload

---
+++ src/test/java/TestClearEnemyWaypoint.java
@@ -0,0 +1,42 @@
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+public class TestClearEnemyWaypoint {
+    @Test
+    public void VisibilityTest() {
+        Tower firetower = new IceTower();
+        firetower.setDamage(3);
+        Text fire = new Text("-" + firetower.getDamage());
+        fire.setFill(Color.RED);
+        fire.setFont(new Font(20));
+
+        Tower earthtower = new IceTower();
+        earthtower.setDamage(2);
+        Text earth = new Text("-" + earthtower.getDamage());
+        earth.setFill(Color.RED);
+        earth.setFont(new Font(20));
+
+        Tower icetower = new IceTower();
+        icetower.setDamage(1);
+        Text ice = new Text("-" + icetower.getDamage());
+        ice.setFill(Color.RED);
+        ice.setFont(new Font(20));
+
+        assertNotEquals(new Text("-" + 3), fire);
+        assertNotEquals(new Text("-" + 2), earth);
+        assertNotEquals(new Text("-" + 1), ice);
+    }
+
+    @Test
+    public void ClearEnemyWaypoint() {
+        ArrayList<com.almasb.fxgl.entity.Entity> enemies = new ArrayList<>(TowerDefense.getEnemies());
+        TowerDefense.setEnemies(new ArrayList<>());
+        assertEquals(enemies, TowerDefense.getEnemies());
+    }
+}
